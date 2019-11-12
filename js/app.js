6413145c00f4f6d5a9002ec6de2ed473a0200ac2
Abi Vickery <vickeryabi@gmail.com>
2019-11-12 14:50:00 -0600

    stuff

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var itemOne = document.getElementById('one');
+itemOne.classList.add('foo');
+itemOne.style.color = 'red';
+itemOne.innerText = 'hello';
+var temp2 = itemOne.innerText;
+console.log(temp2);
+var foo3 = document.querySelector('li.foo');
+console.log(foo3);
+
+var foo4 = document.getElementsByClassName('foo');
+console.log(foo4);
+// foo4.foreach(function(el){
+//     console.log(el);
+// });
+
+var foo5 = document.getElementsByTagName('li');
+console.log(foo5);
+console.log(foo5.item(1));
+
+var foo6 = document.querySelectorAll('.foo');
+console.log(foo6);
+console.log(foo6.item(1));
+
